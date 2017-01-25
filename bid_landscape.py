cbfd83cc5ae2dba9673b45f8df4db50ecfb66240
seg2151 <seg2151@gmail.com>
2017-01-25 17:27:28 -0500

    Add files via upload

---
+++ bid_landscape.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 30 11:44:07 2016
+
+@author: sgarcia
+"""
+filename = 'Krush.xlsx'
+from bid_landscape_sql_scripts import scripts
+import pandas as pd
+from impala.dbapi import connect
+from impala.util import as_pandas
+print('Connecting to host...')
+conn = connect(host = 'lga-impala.pulse.prod', port = 21050)
+cursor = conn.cursor()
+df_list = []
+for script in scripts:
+    print('Executing script...')
+    cursor.execute(script)
+    print('SQL script exectued')
+    df_list.append(as_pandas(cursor))
+print('Merging dataframes')
+df = df_list[0].merge(df_list[1], on='accountname')
+df['bidcpm'] = df.bidprice/df.bidsmade*1000
+writer = pd.ExcelWriter(filename)
+df.to_excel(writer, 'Sheet1')
+df_list[2].to_excel(writer, 'Sheet2')
+writer.save()
+
